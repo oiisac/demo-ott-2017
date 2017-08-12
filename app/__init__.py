801eb7a0426c11ae5966ebe38e521514b26fe0f5
Helgi Oiisac <helgi@oiisac.com>
2017-08-12 10:29:35 +0300

    Bump version: 0.0.0 → 0.0.1

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,2 @@
-PACKAGE_VERSION = "0.0.0"
+PACKAGE_VERSION = "0.0.1"
 APP_NAME = 'demo_task_for_ott'
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 
-version = "0.0.0"
+version = "0.0.1"
 
 
 install_requires = [
