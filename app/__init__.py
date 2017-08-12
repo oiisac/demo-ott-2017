780ef10d6f9890f17c3b5f670cbf3c8adaca1327
Helgi Oiisac <helgi@oiisac.com>
2017-08-12 16:15:41 +0300

    Bump version: 0.0.1 → 0.1.0

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-PACKAGE_VERSION = "0.0.1"
+PACKAGE_VERSION = "0.1.0"
 APP_NAME = 'demo_task_for_ott'
 
 REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 
-version = "0.0.1"
+version = "0.1.0"
 
 
 install_requires = [
